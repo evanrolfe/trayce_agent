844d4e8fa4d1d23ee5e5598dc23bbaa9e0c1adb1
Evan Rolfe <accounts@evanrolfe.me>
2023-06-25 09:46:01 +0100

    code cleanup

--- tc.bpf.c
+++ tc.bpf.c
@@ -46,19 +46,25 @@ struct {
 
 SEC("tc")
 int tc_egress(struct __sk_buff *skb) {
+  if (skb->len > 0)
+    bpf_skb_pull_data(skb, skb->len);
+
+  // ===========================================================================
+  // Get the new skb:
 	void *data = (void *)(long)skb->data;
 	void *data_end = (void *)(long)skb->data_end;
 
   struct ethhdr *eth = data;
   struct iphdr *ip;
-  struct tcphdr *tcp;
 
   if ((void *)eth + sizeof(*eth) > data_end)
       return TC_ACT_OK;
 
   ip = data + sizeof(*eth);
-  if ((void *)ip + sizeof(*ip) > data_end)
-      return TC_ACT_OK;
+  if ((void *)ip + sizeof(*ip) > data_end) {
+    // bpf_printk("RETURN: ip + sizeof(*ip) > data_end (ip_len: %d)", ip_len);
+    return TC_ACT_OK;
+  }
 
   if (ip->protocol != IPPROTO_TCP)
       return TC_ACT_OK;
@@ -67,18 +73,16 @@ int tc_egress(struct __sk_buff *skb) {
     bpf_printk("TODO: Handle IPV6!");
     return TC_ACT_OK;
   }
-  bpf_printk("---------------------------------------------------------------");
-  // bpf_printk("0. skb->len = %d, skb->data = %d, skb->data_end = %d", skb->len, skb->data, skb->data_end);
+
   // Calculate the length of the TCP header
   int ip_len = bpf_ntohs(ip->tot_len);
   int ip_hdr_len = ip->ihl * 4;
-  bpf_skb_pull_data(skb, skb->len);
 
+  bpf_printk("---------------------------------------------------------------");
   bpf_printk("ip_len: %d", ip_len);
-  // bpf_printk("1. skb->len = %d, skb->data = %d, skb->data_end = %d", skb->len, skb->data, skb->data_end);
 
-  // Check if the packet has a payload
-  if (ip_len <= 0) {
+  // TODO: Check if the packet has a payload instead of using arbitrary 80 const value here
+  if (ip_len <= 80) {
     bpf_printk("RETURN: ip_len <= 0");
     return TC_ACT_OK;
   }
@@ -89,30 +93,14 @@ int tc_egress(struct __sk_buff *skb) {
     copy_len = 400;
   }
 
-  // ===========================================================================
-  // Get the new skb:
-	void *data2 = (void *)(long)skb->data;
-	void *data_end2 = (void *)(long)skb->data_end;
-
-  struct ethhdr *eth2 = data2;
-  struct iphdr *ip2;
-
-  if ((void *)eth2 + sizeof(*eth2) > data_end2)
-      return TC_ACT_OK;
-
-  ip2 = data2 + sizeof(*eth2);
-  if ((void *)ip2 + sizeof(*ip2) > data_end2) {
-    bpf_printk("RETURN: ip2 + sizeof(*ip2) > data_end2 (ip_len: %d)", ip_len);
-    return TC_ACT_OK;
-  }
-
-  if ((void *)ip2 + copy_len > data_end2) {
+  if ((void *)ip + copy_len > data_end) {
     bpf_printk("RETURN: Payload out of bounds! ip_len: %d, ip_hdr_len: %d", ip_len, ip_hdr_len);
     return TC_ACT_OK;
   }
 
+  // TODO: Split the ip packet into chunks and send one-at-a-time:
   __u8 my_payload[400] = {0};
-  bpf_probe_read_kernel(&my_payload, copy_len, ip2);
+  bpf_probe_read_kernel(&my_payload, copy_len, ip);
   bpf_perf_event_output(skb, &tcp_payloads, BPF_F_CURRENT_CPU, &my_payload, copy_len);
 
   bpf_printk("Got a request");
@@ -121,15 +109,9 @@ int tc_egress(struct __sk_buff *skb) {
 
 char __license[] SEC("license") = "GPL";
 
-  // // Access the UDP length
-  // __u16 udp_len = bpf_ntohs(udp->len);
-
-  // // Extract individual decimal digits from UDP length
-  // int digit3 = udp_len / 1000;         // Thousands digit
-  // int digit2 = (udp_len / 100) % 10;   // Hundreds digit
-  // int digit1 = (udp_len / 10) % 10;    // Tens digit
-  // int digit0 = udp_len % 10;           // Units digit
-
-  // // Trace print the human-readable decimal digits
-  // bpf_printk("UDP source: %lu, len: %d", udp->source, digit3);
-  // bpf_printk("UDP len: %d%d%d\n", digit2, digit1, digit0);
+// for (unsigned int i = 1; i < 3; i++){
+//   unsigned int offset = i * 50;
+//   struct iphdr *ip_offset = (struct iphdr *)((void *)ip + offset);
+//   bpf_probe_read_kernel(&my_payload, copy_len, ip);
+//   bpf_perf_event_output(skb, &tcp_payloads, BPF_F_CURRENT_CPU, &my_payload, copy_len);
+// }
