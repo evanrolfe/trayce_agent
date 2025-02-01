19d35cf8a14261c20b1dbfcdfff3e60667dba015
Evan Rolfe <accounts@evanrolfe.me>
2025-02-01 10:37:29 +0000

    increase loadtest timeout

--- test/helpers.go
+++ test/helpers.go
@@ -113,7 +113,7 @@ func getTestConfig() (int, int, time.Duration) {
 		timeout = 5 * time.Second
 	} else {
 		numRequests = numRequestsLoad
-		timeout = 30 * time.Second
+		timeout = 60 * time.Second
 	}
 
 	return numRequests, numRequests * 2, timeout
