e6c2b7525dabced7a89e1c4ba0758cb055a0cde2
Evan Rolfe <accounts@evanrolfe.me>
2025-04-08 20:29:47 +0100

    release: 1.0.2

--- cmd/trayce_agent/main.go
+++ cmd/trayce_agent/main.go
@@ -32,7 +32,7 @@ const (
 	btfFilePath6      = "bundle/6.2.0-26-generic.btf"
 	sslLibDefault     = "/usr/lib/x86_64-linux-gnu/libssl.so.3"
 	grpcServerDefault = "localhost:50051"
-	version           = "1.0.1"
+	version           = "1.0.2"
 )
 
 type Settings struct {
